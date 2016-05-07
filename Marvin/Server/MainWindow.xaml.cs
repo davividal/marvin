c9b37e2e2f0346ac2e55de025b63c5e882c27987
Davi Koscianski Vidal <davividal@gmail.com>
2016-05-07 16:30:44 -0300

    Fixing using

--- Marvin/Server/MainWindow.xaml.cs
+++ Marvin/Server/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using Marvin.Domain.Entities;
 
 namespace Marvin
 {
